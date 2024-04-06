8c6e518ce21b624a87e8702d7ecc3ef7a3492338
jerome <zachariah11jerobert@gmail.com>
2024-04-06 12:10:32 +0530

    GetAllJobs & CreateJob done using Controller

---
+++ src/main/java/com/embarkx/firstjobapp/FirstJobAppApplication.java
@@ -0,0 +1,13 @@
+package com.embarkx.firstjobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJobAppApplication.class, args);
+	}
+
+}
