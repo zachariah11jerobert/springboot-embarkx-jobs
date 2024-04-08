7d809ff2058e19184b750d7603a2fd273ada3e70
jerome <zachariah11jerobert@gmail.com>
2024-04-08 12:59:48 +0530

    ResponseEntity Added to written controllers & Constructor injection done

--- src/main/java/com/embarkx/firstjobapp/job/JobController.java
+++ src/main/java/com/embarkx/firstjobapp/job/JobController.java
@@ -1,34 +1,38 @@
 package com.embarkx.firstjobapp.job;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class JobController {
 
-    @Autowired
+
     private JobService jobService;
 
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
     @GetMapping("/jobs")
-    public List<Job> findAll() {
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job) {
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id) {
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
         Job job = jobService.getJobById(id);
         if (job != null)
-            return job;
-        return new Job(1L, "TestJob", "TestJob", "2000", "2000", "Loc");
-
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
